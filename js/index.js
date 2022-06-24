7fc772d8e36d97043ecf3330ed211f80af496a9d
markjakearzadon <95150134+markjakearzadon@users.noreply.github.com>
2022-06-24 12:53:24 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function openbtn() {
+    document.getElementById('dropdown').style.height = "60%";
+    
+}
+
+
+function closebtn() {
+    document.getElementById('dropdown').style.height = "0";
+    
+}
